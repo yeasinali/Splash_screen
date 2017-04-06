b9005d5ca678b93eedb8c6b0dd84bf2079d7e2a0
yeasinali <yeasin1994@gmail.com>
2017-04-06 07:58:16 +0600

    Initial commit

---
+++ app/src/main/java/com/food/recipe/splash_screen/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.food.recipe.splash_screen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(6000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+    }
+
